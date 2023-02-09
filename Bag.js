0781633905b6e03b1d4819b59870a4d84dc61dfe
vee-el <veronicalennon@live.com>
2023-02-09 00:21:18 +0000

    complete

--- Bag.js
+++ Bag.js
@@ -1,8 +1,19 @@
+const express = require("express");
+
 class Bag {
     // Include constructor Method with the following property: 
-        // 1. Weight
+        // 1. Weight            
+    constructor(weight) {
+        if(!weight) {
             //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+            throw new Error("bag needs weight")
+        }
+
+        this.weight = weight;
+    }
 }
 
+
 //Export the module
+
+module.exports = Bag;
--- Person.js
+++ Person.js
@@ -1,10 +1,19 @@
 class Person {
+
     // Include a Constructor Method for Properties: 
         // 1. Name
         // 2. Bags
-            // 2a. Bags should contain/reference a data type for storing more than one bag object.
+    constructor(name, bags) {
+        this.name = name;
+        // 2a. Bags should contain/reference a data type for storing more than one bag object.
+        this.bags = [];
+    }  
 
     // Include an addBag method
+    addBag(bag) {
+        this.bags.push(bag);
+    }
 }
 
 //Export the Module
+module.exports = Person;
